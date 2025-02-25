d54ae3695439b615dc9c68f4be573c5d8e4e1ea7
vasanthakumar <113460093+vasanthakumar-jpg@users.noreply.github.com>
2025-02-25 15:59:20 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     hmr: {
+//         overlay: true,
+//     },
+// },
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-        overlay: true,
-    },
-},
-})
-
+  base: "/OrthoberryCard/", // 👈 Set this to match your repo name
+});
